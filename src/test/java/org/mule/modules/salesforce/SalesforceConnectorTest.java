6bed8c79d3a6563ddc43dd4568132eb9a307b669
Mike Cantrell <mike@devnull.org>
2013-07-13 11:35:12 -0500

    added copyright to test case

--- src/test/java/org/mule/modules/salesforce/SalesforceConnectorTest.java
+++ src/test/java/org/mule/modules/salesforce/SalesforceConnectorTest.java
@@ -1,3 +1,14 @@
+/**
+ * Mule Salesforce Connector
+ *
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+
 package org.mule.modules.salesforce;
 
 import com.sforce.soap.partner.sobject.SObject;
@@ -5,7 +16,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
